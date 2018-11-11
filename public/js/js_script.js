f3bb1e406f1969d3751acc442f1e5aa87dd9bf11
Michael Winton <wintonmich1@ecs.vuw.ac.nz>
2018-11-11 18:38:55 +0100

    dynamic grid

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -1,20 +1,24 @@
-function MenuItem(n, kC, g, l) {
+function MenuItem(n, kC, g, l, img, s) {
     this.name = n; // The this keyword refers to the object itself
     this.kCal = kC;
     this.gluten = g;
     this.lactose = l;
+    this.stock = s;
+    this.image = img
+
     this.nameAndKCal = function() {
         return this.name + ' ' + this.kCal;
     };
+
 }
 
 // Objects are then instantiated using the new keyword
 
 var burgers = [ 
-    new MenuItem('Burger 1', '2.4kCal', 'Gluten', 'Lactose'), 
-    new MenuItem('Burger 2', '1.7kCal', 'Gluten Free', 'Lactose'), 
-    new MenuItem('Burger 3', '1.9kCal', 'Gluten', 'Lactose'),
-    new MenuItem('Milkshake', '1.5kCal', 'Gluten', 'Lactose'),
-    new MenuItem('Smoothie', '0.7kCal', 'Gluten', 'Lactose Free')
+    new MenuItem('Burger 1', '2.4kCal', true, false, 'http://www.designindaba.com/sites/default/files/styles/scaledlarge/public/node/news/23566/sonic-burger.jpg?itok=zGk5pjcI',  1), 
+    new MenuItem('Burger 2', '1.7kCal', false, true, ' ', 3), 
+    new MenuItem('Burger 3', '1.9kCal', true , true, ' ', 19),
+    new MenuItem('Milkshake', '1.5kCal', true , true, ' ', 0),
+    new MenuItem('Smoothie', '0.7kCal', true ,false, ' ', 0)
 ]
 
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -2,7 +2,7 @@ var vm = new Vue({
     //Here comes the optional elements of the Vue object
     el: '#myID', 
     data: {
-        burgerName: 'Burger Name 1'
+        burgerName: burgers
     }
 })
 
